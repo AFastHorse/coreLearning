bc63a190b67ce8b117b96e15acb2b885fe7fec1b
Gaoyuwei <gaoyuwei132@qq.com>
2018-12-11 15:13:10 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace HelloWorld
 {
@@ -6,7 +9,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            test_mult_insert();
+        }
+        static void test_mult_insert()
+        {
+            List<Content> contents = new List<Content>()
+            {
+                new Content{title="批量插入标题1",content="Contetn1"},
+                new Content{title="Mult2",content="Content2" }
+            };
+            using (var conn = new SqlConnection("server=.;database=CMS;integrated security=true;"))
+            {
+                string sql = @"Insert into content(title,content,status,add_time,modify_time)
+                               values(@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql, contents);
+                Console.WriteLine($"test_mult_insert:Insert {result} datas!");
+            }
         }
     }
 }
